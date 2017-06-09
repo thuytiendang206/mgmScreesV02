0a32daa4538d320f8757256a1b03e7b2dffd05d2
languyen <wildboar.work@gmail.com>
2017-06-09 09:34:13 +0700

    1st commit for SCREENS-11
    Change-Id: I5fc0819d157318b4dcfd0f5f8c18a4c50e5fb3ae

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+ import Hello from './components/Hello'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Hello text="Hello World"></Hello>
+      </div>
+    );
+  }
+}
+
+export default App;
