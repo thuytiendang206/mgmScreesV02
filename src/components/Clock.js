e5f87d35c870b90f1ef4c6a444b509fd0f1fdea2
“Tien <thuytien@Macintoshs-MacBook-Air.local>
2017-06-14 11:47:57 +0700

    add clock-screens
    Change-Id: I1f3a75e49a9b8fbe2499c303def0cf62869d637b

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,32 +1,41 @@
 import React from 'react';
+import Analog from './Analog'
 
 class Clock extends React.Component{
+
     constructor(props){
         super(props);
-        this.state = {
-            time : Date().toString()
-        }
+        this.state =({
+            time : new Date().toLocaleTimeString(),
+            date : new Date().toDateString()
+        });
     }
     static getType() {
         return "Clock";
     }
-    render(){
-        return (
-            <p className="App-clock">
-                The time is {this.state.time}
-            </p>
-        );
-    }
-    componentDidMount() {
-        this.intervalID = setInterval(() => this.tick(),1000);
-    }
-    componentWillUnmount() {
-        clearInterval(this.intervalID);
-    }
-    tick(){
+    incrementCounter(){
+        var options ={ hour12: true};
         this.setState({
-            time : new Date().toString()
+            time : new Date().toLocaleTimeString('en-US',options),
+            date : new Date().toDateString()
         });
+    }
+
+    componentDidMount(){
+        this.timerID = setInterval(() => this.incrementCounter(),1000);
+    }
+    componentWillUnmount(){
+        clearInterval(this.timerID); 
+    }
+    
+    render(){
+        return(
+            <div className="clock">
+                <Analog width='400' height='400'/>
+                <h2 className="time"> {this.state.time} </h2>
+                <h2 className="date"> {this.state.date}</h2>
+            </div>   
+        );
     }
 }
 
