48637e9ca57c86f8625a309bf9a9386831748f7b
languyen <wildboar.work@gmail.com>
2017-07-10 14:18:38 +0700

    Screens-70, Make the calendar screens easier to see, make the calendar's JSON format fixed with back-end
    Change-Id: Ie57a5a39878472432d8e116cab4a8a3592efd9b3

--- frontend/src/components/calendar/Calendar.js
+++ frontend/src/components/calendar/Calendar.js
@@ -1,6 +1,10 @@
-import React from 'react'
-import CalendarGUI from './CalendarGUI.js'
-import axios from 'axios'
+import React from 'react';
+import axios from 'axios';
+
+import CalendarGUI from './CalendarGUI.js';
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import './Calendar.css';
 
 class Calendar extends React.Component {
   constructor(props) {
@@ -17,19 +21,19 @@ class Calendar extends React.Component {
 
   //get full URL with Google API
   getURl(id) {
-    return 'https://www.googleapis.com/calendar/v3/calendars/' + id 
-    + '/events?key=' 
-    + 'AIzaSyDyWLiA-Jd0WcDsbedpw-7I8dp-rdinktA';
+    return 'https://www.googleapis.com/calendar/v3/calendars/' + id
+      + '/events?key='
+      + 'AIzaSyDyWLiA-Jd0WcDsbedpw-7I8dp-rdinktA';
   }
 
   //transform data
-  fetchData(response,color) {
+  fetchData(response, color) {
     let items = response.data.items;
     let ret = [];
     let desc = undefined;
     let obj = {};
-    for (let j = 0;j < items.length; j++){
-      let item=items[j];
+    for (let j = 0; j < items.length; j++) {
+      let item = items[j];
       // just adding the first line of description
       if (item.description !== undefined) {
         desc = item.description.split('\n')[0];
@@ -40,35 +44,41 @@ class Calendar extends React.Component {
         'end': new Date(item.end.dateTime),
         'desc': desc,
         'color': color,
+        'time-range': this.props.params['time-max'] - this.props.params['time-min'],
       };
-      desc=undefined;
+      desc = undefined;
       ret.push(obj);
     }
-      return ret;
+    return ret;
   }
 
-  componentWillMount() {
+  componentDidMount() {
     let events = [];
     let arr = [];
-    for(let i=1;i<=this.props.params['number-of-calendars'];i++){
-      let obj={
-        'calendar-id':this.props.params['calendar-id-'+i],
-        'color':this.props.params['color-'+i],
-      };
-      arr.push(obj);
+    for (let i = 1; i <= this.props.params['number-of-calendars']; i++) {
+      let item = {
+        'calendar-id': this.props.params['calendar-id-' + i],
+        'color': this.props.params['color-' + i],
+      }
+      arr.push(item);
     }
-    arr.map((item) =>{
+    arr.map((item) => {
       return axios.get(this.getURl(item['calendar-id']))
-      .then(response=>{
-        events=[...events,...this.fetchData(response,item['color'])];
-        this.setState({ events: events });
-      });
+        .then(response => {
+          events = [...events, ...this.fetchData(response, item['color'])];
+          this.setState({ events: events });
+        });
     });
   }
 
   render() {
     return (
-      <CalendarGUI events={this.state.events} name={this.props.params.name}></CalendarGUI>
+      <CalendarGUI events={this.state.events}
+        name={this.props.params.name}
+        timeMin={this.props.params['time-min']}
+        timeMax={this.props.params['time-max']}
+      >
+      </CalendarGUI>
     )
 
   }
--- frontend/src/components/calendar/CalendarGUI.js
+++ frontend/src/components/calendar/CalendarGUI.js
@@ -1,25 +1,54 @@
-import React from 'react'
-import moment from 'moment'
+import React from 'react';
+import moment from 'moment';
 import BigCalendar from 'react-big-calendar';
 
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import './Calendar.css';
-
-
 class CalendarGUI extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     BigCalendar.momentLocalizer(moment);
   }
 
   //adding description for each event
   Event({ event }) {
+    // function finding the longest word in a string
+    let findLongestWord = function (str) {
+      var words = str.split(' ');
+      var maxLength = 0;
+
+      for (var i = 0; i < words.length; i++) {
+        if (words[i].length > maxLength) {
+          maxLength = words[i].length;
+        }
+      }
+      return maxLength;
+    }
+    // find the max between 2 numbers    
+    let maxNumber = function (a, b) {
+      return (a > b) ? a : b;
+    };
+    // find the min between 2 numbers    
+    let minNumber = function (a, b) {
+      return (a < b) ? a : b;
+    };
+    // screenValue is the smaller dimession of window
+    let screenValue = minNumber(window.innerHeight, window.innerWidth);
+    let fontSize = event.end.getHours() - event.start.getHours();
+    //if the duration time of event < 3 hours, the description will not be shown
+    if (fontSize < 3) {
+      event.desc = undefined;
+    }
+    //need value of time range for calendar screens here (e.g. 11 hours -from 8-19)
+    fontSize = maxNumber(screenValue * 0.04, (screenValue * 0.16 * fontSize) / (event['time-range']));
+    fontSize = minNumber(window.innerWidth * 0.23 / findLongestWord(event.title), fontSize);
     return (
+      // the fontSize of description will smaller than title fontSize of title
       <span>
-        <strong>
-        {event.title}
-        </strong>
-        { event.desc && (':  ' + event.desc)}
+        <strong style={{ fontSize: fontSize + 'px', fontFamily: '-webkit-body' }} >
+          {event.title}
+        </strong><br />
+        <span style={{ fontSize: fontSize / 2 + 'px' }}>
+          {event.desc && (':  ' + event.desc)}
+        </span>
       </span>
     )
   }
@@ -27,39 +56,42 @@ class CalendarGUI extends React.Component {
   //changing the color for each event depending on type
   eventStyleGetter(event) {
     //choosing the background color
-    var backgroundColor = event.color;
-    var style = {
-        backgroundColor: backgroundColor,
-        borderRadius: '0px',
-        opacity: 0.8,
-        color: 'black',
-        border: '0px',
-        display: 'block'
+    let backgroundColor = event.color;
+    let style = {
+      backgroundColor: backgroundColor,
+      borderRadius: '0px',
+      border: '0px',
+      color: 'black',
+      display: 'block',
+      opacity: 0.8
     };
     return {
-        style: style
+      style: style
     };
   }
 
   //render GUI
-    render(){
-        let allViews = Object.keys(BigCalendar.views).map(k => BigCalendar.views[k]);
+  render() {
+    let allViews = Object.keys(BigCalendar.views).map(k => BigCalendar.views[k]);
     return (
       <div className='calendar-content'>
-        <span className="rbc-toolbar-label">Name: {this.props.name}</span>
+        <span className="calendar-name">Name: {this.props.name}</span>
         <BigCalendar
-        {...this.props}
-        events={this.props.events}
-        views={allViews}
-        defaultDate={new Date()}
-        //showing only mon to fri in calendar
-        defaultView='work_week'
-        toolbar={false}
-        //it will receive property for each event in the array auto
-        components={{
-          event : this.Event,
-        }}
-        eventPropGetter={(this.eventStyleGetter)}
+          {...this.props}
+          events={this.props.events}
+          views={allViews}
+          defaultDate={new Date()}
+          //showing only mon to fri in calendar
+          defaultView='work_week'
+          toolbar={false}
+          //it will receive property for each event in the array auto
+          components={{
+            event: this.Event,
+          }}
+          eventPropGetter={(this.eventStyleGetter)}
+          //set limit time range for calendar
+          max={new Date(1995, 12, 12, this.props.timeMax)}
+          min={new Date(1995, 12, 12, this.props.timeMin)}
         />
       </div>
     )
