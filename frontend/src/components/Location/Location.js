6db7d589f7d67302abe8ec488ad6ebb059320c64
bphan <Binh.Phan@mgm-tp.com>
2017-07-11 09:03:43 +0700

    fix es6 issue by rewriting function using old syntax and polyfill Math.trunc
    Change-Id: I8dd4899507573a788c213c661fc4b72d0e655a05

--- frontend/src/components/Location/Location.js
+++ frontend/src/components/Location/Location.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import TogglePages from '../togglePages/TogglePages.js';
 import Error from '../error404/Error';
 import axios from 'axios';
 class Location extends Component {
-    constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
     this.state = {
       errorOccured: false
     }
@@ -13,37 +13,45 @@ class Location extends Component {
   fetchData() {
     axios.get(this.geturl())
       .then(res => {
-        if (typeof(res.data)==="object"){
+        if (typeof (res.data) === "object") {
           this.setState({
             data: res.data
           });
         }
-        else{
-          this.setState({errorOccured: true});
+        else {
+          this.setState({ errorOccured: true });
         }
-      }, () => {this.setState({errorOccured: true});})
+      }, () => { this.setState({ errorOccured: true }); })
   }
 
   geturl() {
-    const defaultScreen= './differentlocationsjson/default.json';
-    const url = new URL(window.location.href);
+    const defaultScreen = './differentlocationsjson/default.json';
+    const url = window.location.href;
     const substring = "screenplay=";
-    if (!url.search) return defaultScreen;
+    if (url.indexOf(substring) == -1) return defaultScreen;
     else {
-      if (window.location.href.includes(substring)){
-        var params = url.searchParams.get("screenplay");
-        params='./differentlocationsjson/'+params+'.json';
-        return params;
-      }
+      var params = this.getQueryVariable("screenplay");
+      params = './differentlocationsjson/' + params + '.json';
+      return params;
     }
   }
-
+  getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+      if (decodeURIComponent(pair[0]) == variable) {
+        return decodeURIComponent(pair[1]);
+      }
+    }
+    console.log('Query variable %s not found', variable);
+  }
   render() {
-    const component = this.state.errorOccured 
-          ? <Error /> 
-          : this.state.data 
-            ? <TogglePages url={this.state.data} /> 
-            : undefined;
+    const component = this.state.errorOccured
+      ? <Error />
+      : this.state.data
+        ? <TogglePages url={this.state.data} />
+        : undefined;
     return (
       <div className="App">
         {component}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './es6-polyfill';
 import App from './components/app/App';
-import registerServiceWorker from './registerServiceWorker';
 import './static/css/bootstrap3_3_7.min.css';
 import './index.css';
-
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
