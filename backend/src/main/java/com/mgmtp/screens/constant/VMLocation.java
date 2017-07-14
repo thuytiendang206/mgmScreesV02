dce0cdda3b082fabec185a57bb39bf343d675c10
bphan <Binh.Phan@mgm-tp.com>
2017-07-14 14:05:25 +0700

    Fix CORS configs
    Change-Id: I2d749601d8dadef79e63655971443d086011355e

--- backend/src/main/java/com/mgmtp/screens/constant/VMLocation.java
+++ backend/src/main/java/com/mgmtp/screens/constant/VMLocation.java
@@ -2,8 +2,8 @@ package com.mgmtp.screens.constant;
 
 public class VMLocation {
 	public static final String LOCAL_URL = "http://localhost:3000";
-	public static final String DEV_URL = "http://devscreens/";
-	public static final String PROD_URL = "http://prodscreens/";
+	public static final String DEV_URL = "http://devscreens";
+	public static final String PROD_URL = "http://prodscreens";
 
 	private VMLocation() {
 	}
