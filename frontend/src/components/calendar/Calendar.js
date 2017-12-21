3c948a98695699ea6d28021e00d494e97d9aaba4
“Tien <thuytiendang206@gmail.com>
2017-12-21 17:32:59 +0700

    SCREENS-79 Analyze code to refactor -- Sub task : SCREENS-86 : Analyze code for Calendar page    1. Fixed link get API    2. Render 1 time if data don't change    3. Fixed bug : "no title"    4. Styling a litle ^.^
    Change-Id: I7eaccabb33e66cc87aa39929937d8a10940f529b

--- frontend/src/components/calendar/Calendar.js
+++ frontend/src/components/calendar/Calendar.js
@@ -1,140 +1,73 @@
 import React from 'react';
 import axios from 'axios';
 import CalendarGUI from './CalendarGUI';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import './Calendar.css';
+import moment from 'moment-timezone';
+import {transformData} from './action';
 
+const rootUrl = 'https://www.googleapis.com/calendar/v3/calendars/';
+const searchEvent = '/events?key=';
+const key = 'AIzaSyDyWLiA-Jd0WcDsbedpw-7I8dp-rdinktA';
 class Calendar extends React.Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      events: [],
-    };
-  }
 
-  //fix the minify problem
-  static getType() {
-    return "Calendar";
-  }
-
-  //get full URL with Google API
-  getUrl(id) {
-    return 'https://www.googleapis.com/calendar/v3/calendars/' + id
-      + '/events?key='
-      + 'AIzaSyDyWLiA-Jd0WcDsbedpw-7I8dp-rdinktA';
-  }
-
-  //transform data
-  fetchData(response, color) {
-    let items = response.data.items;
-    let ret = [];
-    let desc = undefined;
-
-    for (let j = 0; j < items.length; j++) {
-      let item = items[j];
-      // just adding the first line of description
-      if (item.description !== undefined) {
-        desc = item.description.split('\n')[0];
-      }
-      let objEvent = {
-        'title': item.summary,
-        'start': undefined,
-        'end': undefined,
-        'desc': desc,
-        'color': color,
-        'allDay': false,
-        'time-range': this.props.params['time-max'] - this.props.params['time-min']
-      };
-      let start = new Date(item.start.dateTime || item.start.date);
-      let end = new Date(item.end.dateTime || item.end.date);
-      objEvent.allDay = (item.start.dateTime === undefined) || (item.end.dateTime === undefined);
-      let datePlus = new Date(start.getFullYear(), start.getMonth(), start.getDate() + 1, start.getHours());
-      let islager2days = end.getDate() >= datePlus.getDate();
-      // push event if it is evenr happen all day (without hour star or end )
-      if (objEvent.allDay) {
-        // set time : minus 1 day
-        end.setTime(end.getTime() - 86400000);
-        objEvent.start = start;
-        objEvent.end = end;
-        ret.push(objEvent);
-      }
-      // if events last than than a day , split event : event for date start, events for the day between day start and day end , envent for date end 
-      else if (islager2days) {
-        // push if event date start in time range
-        if (start.getHours() < this.props.params['time-max'] && start.getHours() > this.props.params['time-min']) {
-          objEvent.start = new Date(item.start.dateTime);
-          objEvent.end = new Date(start.getFullYear(), start.getMonth(), start.getDate(), this.props.params['time-max']);
-          ret.push(objEvent);
-        }
-        // push if event date end in time range
-        if (end.getHours() <= this.props.params['time-max'] && end.getHours() > this.props.params['time-min']) {
-          let objEvent2 = {
-            'title': item.summary,
-            'start': new Date(end.getFullYear(), end.getMonth(), end.getDate(), this.props.params['time-min']),
-            'end': new Date(item.end.dateTime),
-            'desc': desc,
-            'color': color,
-            'time-range': this.props.params['time-max'] - this.props.params['time-min']
-          }
-          ret.push(objEvent2);
-        }
-        // push all event for the middle days if exist
-        while (datePlus.getDate() !== end.getDate()) {
-          let objEvent3 = {
-            'title': item.summary,
-            'start': new Date(datePlus.getFullYear(), datePlus.getMonth(), datePlus.getDate(), this.props.params['time-min']),
-            'end': new Date(datePlus.getFullYear(), datePlus.getMonth(), datePlus.getDate(), this.props.params['time-max']),
-            'desc': desc,
-            'color': color,
-            'time-range': this.props.params['time-max'] - this.props.params['time-min']
-          }
-          ret.push(objEvent3);
-          datePlus.setDate(datePlus.getDate() + 1);
-        }
-      }
-      // push event if the start and end in the same day
-      else if (end.getDate() === start.getDate()) {
-        if (end.getHours() > this.props.params['time-min'] && start.getHours() < this.props.params['time-max']) {
-          objEvent.start = start;
-          objEvent.end = end;
-          ret.push(objEvent);
-        }
-      }
-      desc = undefined;
+    constructor(props) {
+        super(props);
+        this.state = {
+            events: [],
+        };
     }
-    return ret;
-  }
 
-  componentDidMount() {
-    let events = [];
-    let arr = [];
-    for (let i = 1; i <= this.props.params['number-of-calendars']; i++) {
-      let item = {
-        'calendar-id': this.props.params['calendar-id-' + i],
-        'color': this.props.params['color-' + i],
-      }
-      arr.push(item);
+    getUrl(id, timeMin) {
+        return rootUrl + id + searchEvent + key + timeMin;
     }
-    arr.map((item) => {
-      return axios.get(this.getUrl(item['calendar-id']))
-        .then(response => {
-          events = [...events, ...this.fetchData(response, item['color'])];
-          this.setState({ events: events });
-        });
-    });
-  }
 
-  render() {
-    return (
-      <CalendarGUI events={this.state.events}
-        name={this.props.params.name}
-        timeMin={this.props.params['time-min']}
-        timeMax={this.props.params['time-max']}
-      >
-      </CalendarGUI>
-    )
-  }
+    getCalendar(props) {
+        let events = [];
+        let arr = [];
+        let events_refined = [];
+        let timeMin = '&timeMin=' + encodeURIComponent(moment().startOf('week').add(1, 'day').format());
+
+        for (let i = 1; i <= props.params['number-of-calendars']; i++) {
+            let item = {
+                'calendar-id': props.params['calendar-id-' + i],
+                'color': props.params['color-' + i]
+            }
+            arr.push(item);
+        }
+        for (let i = 0; i < props.params['number-of-calendars']; i++) {
+            events.push(axios.get(this.getUrl(arr[i]['calendar-id'], timeMin)));
+        }
+        axios.all(events)
+            .then(axios.spread((...events) => {
+                for (let i = 0; i < props.params['number-of-calendars']; i++) {
+                    events_refined = [...events_refined, ...transformData(events[i], arr[i]['color'],this.props.params['time-max'],this.props.params['time-min'])];
+                }
+            }))
+            .then(() => {
+                this.setState({ events: events_refined });
+            });
+    }
+
+    componentDidMount() {
+        this.getCalendar(this.props);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        for (let i = 0; i < this.props.params['number-of-calendars']; i++) {
+            if (JSON.stringify(this.state.events[i]) !== JSON.stringify(nextState.events[i])) return true;
+        }
+        return false;
+    }
+
+    render() {
+        return (
+            <CalendarGUI events={this.state.events}
+                name={this.props.params.name}
+                maxTime={this.props.params['time-min']}
+                minTime={this.props.params['time-max']}
+            >
+            </CalendarGUI>
+        )
+    }
 }
 
 export default Calendar;
--- frontend/src/components/calendar/CalendarGUI.js
+++ frontend/src/components/calendar/CalendarGUI.js
@@ -1,103 +1,70 @@
 import React from 'react';
 import moment from 'moment';
 import BigCalendar from 'react-big-calendar';
+import { minNumber, maxNumber, eventStyleGetter, findLongestWord } from './action';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import './Calendar.css';
+
 
 class CalendarGUI extends React.Component {
-  
-  constructor(props) {
-    super(props);
-    BigCalendar.momentLocalizer(moment);
-  }
 
-  //adding description for each event
-  Event({ event }) {
-    // function finding the longest word in a string
-    let findLongestWord = function (str) {
-      var words = str.split(' ');
-      var maxLength = 0;
+    constructor(props) {
+        super(props);
+        BigCalendar.momentLocalizer(moment);
+    }
 
-      for (var i = 0; i < words.length; i++) {
-        if (words[i].length > maxLength) {
-          maxLength = words[i].length;
+    setEvent({ event }) {
+        let screenValue = minNumber(window.innerHeight, window.innerWidth);
+        let fontSize = event.end.getHours() - event.start.getHours();
+
+        //if the duration time of event < 3 hours, the description will not be shown
+        if (fontSize < 3) {
+            event.desc = undefined;
         }
-      }
-      return maxLength;
-    }
-    // find the max between 2 numbers    
-    let maxNumber = function (a, b) {
-      return (a > b) ? a : b;
-    };
-    // find the min between 2 numbers    
-    let minNumber = function (a, b) {
-      return (a < b) ? a : b;
-    };
-    // screenValue is the smaller dimession of window
-    let screenValue = minNumber(window.innerHeight, window.innerWidth);
-    let fontSize = event.end.getHours() - event.start.getHours();
-    //if the duration time of event < 3 hours, the description will not be shown
-    if (fontSize < 3) {
-      event.desc = undefined;
-    }
-    //need value of time range for calendar screens here (e.g. 11 hours -from 8-19)
-    fontSize = maxNumber(screenValue * 0.04, (screenValue * 0.16 * fontSize) / (event['time-range']));
-    fontSize = minNumber(window.innerWidth * 0.23 / findLongestWord(event.title), fontSize);
-    return (
-      // the fontSize of description will smaller than title fontSize of title
-      <span>
-        <strong style={{ fontSize: fontSize + 'px', fontFamily: '-webkit-body' }} >
-          {event.title}
-        </strong><br />
-        <span style={{ fontSize: fontSize / 2 + 'px' }}>
-          {event.desc && (':  ' + event.desc)}
-        </span>
-      </span>
-    )
-  }
 
-  //changing the color for each event depending on type
-  eventStyleGetter(event) {
-    //choosing the background color
-    let backgroundColor = event.color;
-    let style = {
-      backgroundColor: backgroundColor,
-      borderRadius: '0px',
-      border: '0px',
-      color: 'black',
-      display: 'block',
-      opacity: 0.8
-    };
-    return {
-      style: style
-    };
-  }
+        //need value of time range for calendar screens here (e.g. 11 hours -from 8-19)
+        fontSize = maxNumber(screenValue * 0.04, (screenValue * 0.16 * fontSize) / (event['time-range']));
+        fontSize = minNumber(window.innerWidth * 0.23 / findLongestWord(event.title), fontSize);
 
-  //render GUI
-  render() {
-    let allViews = Object.keys(BigCalendar.views).map(k => BigCalendar.views[k]);
-    return (
-      <div className='calendar-content'>
-        <span className="calendar-name">Name: {this.props.name}</span>
-        <BigCalendar
-          {...this.props}
-          events={this.props.events}
-          views={allViews}
-          defaultDate={new Date()}
-          //showing only mon to fri in calendar
-          defaultView='work_week'
-          toolbar={false}
-          //it will receive property for each event in the array auto
-          components={{
-            event: this.Event,
-          }}
-          eventPropGetter={(this.eventStyleGetter)}
-          //set limit time range for calendar
-          max={new Date(1995, 12, 12, this.props.timeMax - 1, 59)}
-          min={new Date(1995, 12, 12, this.props.timeMin)}
-          endAccessor={({ end }) => new Date(end.getTime() - 1)}
-        />
-      </div>
-    )
-  }
+        return (
+            // the font size of the description will be smaller than title font size of the title
+            <span>
+                <strong style={{ fontSize: fontSize + 'px', color: 'white' }} >
+                    {event.title}
+                </strong><br />
+                <span style={{ fontSize: fontSize / 2 + 'px' }}>
+                    {event.desc && (':  ' + event.desc)}
+                </span>
+            </span>
+        )
+    }
+
+    render() {
+        const allViews = Object.keys(BigCalendar.views).map(k => BigCalendar.views[k]);
+        return (
+            <div className='calendar-content'>
+                <span className="calendar-name">{this.props.name}</span>
+                <BigCalendar
+                    {...this.props}
+                    events={this.props.events}
+                    views={allViews}
+                    defaultDate={new Date()}
+                    //monday to friday
+                    defaultView='work_week'
+                    toolbar={false}
+                    //it will receive property for each event in the array automatically
+                    components={{
+                        event: this.setEvent,
+                    }}
+                    eventPropGetter={(eventStyleGetter)}
+                    //set time range for calendar (from min(hour) to max(hour))
+                    max={new Date(0, 0, 0, this.props.maxTime - 1, 59)}
+                    min={new Date(0, 0, 0, this.props.minTime)}
+                    endAccessor={({ end }) => new Date(end.getTime() - 1)}
+                />
+            </div>
+        )
+    }
 }
 
 export default CalendarGUI;
